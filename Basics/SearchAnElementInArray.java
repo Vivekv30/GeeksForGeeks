324cb74a1e05efc1fdfdd259363c60e0f5251191
Vivekv30 <129572642+Vivekv30@users.noreply.github.com>
2023-09-15 12:40:31 +0530

    Update SearchAnElementInArray.java

--- Basics/SearchAnElementInArray.java
+++ Basics/SearchAnElementInArray.java
@@ -1,15 +1,13 @@
-class Solution{
-        
-    static int search(int arr[], int N, int X)
-    {
+//time complexity - o(n)
+//space complexity - o(1)
+class Solution{     
+    static int search(int arr[], int N, int X){
         
         for(int i=0;i<N;i++){
             if(X==arr[i]){
               return i;
             }
         }
-        return -1;
-        
-    }
-    
+        return -1;     
+    }    
 }
