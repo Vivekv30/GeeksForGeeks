2745037b9521b8504fc16bd85dbe5340c1c12b07
Vivekv30 <129572642+Vivekv30@users.noreply.github.com>
2023-10-06 15:54:12 +0530

    Create SortTheArray.java

---
+++ Basics/SortTheArray.java
@@ -0,0 +1,12 @@
+//time complexity - O(NlogN)
+//space complexity - O(1)
+
+class Solution 
+{ 
+    int[] sortArr(int[] arr, int n) 
+    { 
+        // code here
+        Arrays.sort(arr);
+        return arr;
+    }    
+} 
